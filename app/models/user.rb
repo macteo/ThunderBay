732f7c7b665f8dfcb30f91b52e1a2932d4e9ca98
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2015-12-23 16:24:41 +0100

    User inside json profile payload.

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,9 +40,10 @@ class User < ActiveRecord::Base
   def send_inside_status_change_notification(profile)
     self.devices.each do |device|
       # TODO: need to send a nested object with the user informations
-      jsonProfile = profile.to_json
-      jsonProfile["user"] = self.to_json
-      Message.create(:receiver_id => device.id, :payload => "{\"inside_status_change\":#{jsonProfile}}", :content_available => 1)
+      jsonProfile = profile.attributes
+      jsonProfile["user"] = self
+      logger.info "--------- #{jsonProfile}"
+      Message.create(:receiver_id => device.id, :payload => "{\"inside_status_change\":#{jsonProfile.to_json}}", :content_available => 1)
     end
   end
 end
