ec1d76b70012662b53511c277869ec3ee9334e31
Matteo Gavagnin <matteo.gavagnin@dimension.it>
2015-12-23 10:05:05 +0100

    Requiring websocket rails js before tree.

--- config/puma.rb
+++ config/puma.rb
@@ -16,27 +16,3 @@ on_worker_boot do
     ActiveRecord::Base.establish_connection(config)
   end
 end
-
-# #!/usr/bin/env puma
-#
-# daemonize false
-#
-# pidfile 'tmp/pids/puma.pid'
-# state_path 'tmp/pids/puma.state'
-#
-# threads 0,16
-#
-# preload_app!
-#
-# bind 'unix:///tmp/desman.sock'
-# environment 'production'
-#
-# preload_app!
-#
-# activate_control_app 'unix://tmp/sockets/pumactl.sock'
-#
-# on_worker_boot do
-#   ActiveSupport.on_load(:active_record) do
-#     ActiveRecord::Base.establish_connection
-#   end
-# end
